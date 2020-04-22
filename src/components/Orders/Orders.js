e8e3efb251b3d01425045dbc8c41892171e4201a
Karen Nguyen <knguyen4184@yahoo.com>
2020-04-22 10:07:16 -0600

    remove store from add pass props to Order component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  });
+class Orders extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+  render() {
+    const orderEls = this.props.orders.map(order => {
+      return (
+        <div key={order.id} className="order">
+          <h3>{order.name}</h3>
+          <ul className="ingredient-list">
+            {order.ingredients.map(ingredient => {
+              return <li>{ingredient}</li>
+            })}
+          </ul>
+        </div>
+      )
+    });
+  
+    return (
+      <section >
+        { orderEls.length ? orderEls : <p>No orders yet!</p> }
+      </section>
+    )
+  }
 }
 
-export default Orders;
+const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+const mapDispatchToProps = dispatch => (
+  {
+    setOrders: (orders) => dispatch(setOrders(orders))
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
